7fe5a85aa59a90b7367918d212ee0ddea710b398
John F. Kanthak <johnkanthak@yahoo.com>
2013-05-14 09:25:40 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClassTracker::Application.config.secret_token = '805642d3c189328e1090759ad0ef0d786ef9959dd32c3bd7ca72ee70ec9b43813136b29a59a4d5d89ee265d5e98129cc1fe4cde496f7ab7ae34a9afa579488b9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ClassTracker::Application.config.session_store :cookie_store, key: '_class_tracker_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ClassTracker::Application.config.session_store :active_record_store
